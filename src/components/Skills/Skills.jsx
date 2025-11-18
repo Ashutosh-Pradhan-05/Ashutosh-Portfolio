6c113f7748cc557351d9cb526ba91a3b2f9a1f44
Ashutosh Pradhan <iamashutoshpradhan@gmail.com>
2025-11-18 23:16:56 +0530

    Heading, text and background colors changes in Skills component.

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 // Path main/src/components/Skills/Skills.jsx
 import React from "react";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 import { SkillsInfo } from "../../constants";
 import Tilt from "react-parallax-tilt";
@@ -11,38 +11,43 @@ const Skills = () => (
   >
     {/* Section Title */}
     <div className="text-center mb-8">
-      <h2 className="text-3xl sm:text-4xl font-bold">TECH &nbsp;STACKS</h2>
-      {/* <div className="w-72 h-3 bg-gradient-to-r from-[#fc466b] to-[#3f5efb] mx-auto mt-2"></div> */}
+      <h2 className="text-gray-300 text-3xl sm:text-4xl font-bold">
+        TECH &nbsp;STACKS
+      </h2>
       <motion.div
         animate={{
-          backgroundPosition: ['0% 50%', '100% 50%', '0% 50%'],
+          backgroundPosition: ["0% 50%", "100% 50%", "0% 50%"],
         }}
         transition={{
           duration: 1,
           repeat: Infinity,
-          ease: 'easeInOut',
+          ease: "easeInOut",
         }}
         className="w-72 h-3 mx-auto mt-2 bg-gradient-to-r from-[#fc466b] to-[#3f5efb] bg-[length:200%_200%]"
         style={{
-          backgroundImage: 'linear-gradient(to right, #fc466b, #3f5efb)',
-          backgroundSize: '200% 200%',
-          backgroundRepeat: 'no-repeat',
+          backgroundImage: "linear-gradient(to right, #fc466b, #3f5efb)",
+          backgroundSize: "200% 200%",
+          backgroundRepeat: "no-repeat",
         }}
       />
-      <p className="text-gray-600 mt-4 text-lg font-semibold text-justify sm:text-lg md:text-center">
-        A showcase of my technical competencies and expertise, developed through hands-on experience across diverse projects and real-world applications.      </p>
+      <p className="text-gray-400 mt-4 text-lg font-semibold text-justify sm:text-lg md:text-center">
+        A showcase of my technical competencies and expertise, developed through
+        hands-on experience across diverse projects and real-world applications.{" "}
+      </p>
     </div>
     {/* Skill Categories */}
     <div className="flex flex-wrap gap-1 lg:gap-5 py-10 justify-between">
       {SkillsInfo.map((category) => (
-        <div key={category.title}
-          className="bg-gray-900 backdrop-blur-md px-6 sm:px-10 py-8 sm:py-6 mb-10 w-full sm:w-[48%] rounded-2xl border border-white 
-          shadow-[0_0_20px_1px_rgba(130,69,236,0.3)]">
-          <h3 className="text-2xl sm:text-3xl font-semibold text-gray-400 mb-1 text-center">
+        <div
+          key={category.title}
+          className="bg-radial from-[#050414] from-50% to-[#2c076c] backdrop-blur-md px-6 sm:px-10 py-8 sm:py-6 mb-10 w-full sm:w-[48%] rounded-2xl border border-white 
+          shadow-[0_0_20px_1px_rgba(130,69,236,0.3)]"
+        >
+          <h3 className="text-2xl sm:text-3xl font-semibold text-gray-300 mb-1 text-center">
             {category.title}
           </h3>
           {/* //Horizontal line */}
-          <div className="w-full h-1 bg-gradient-to-r from-[#8245ec] to-[#ff7f50] mx-auto mb-10"></div>
+          <div className="w-full h-1 bg-gradient-to-r from-[#ff7f50] from-1% via-[#8245ec] via-50% to-[#ff7f50] to-99% mx-auto mb-10"></div>
           {/* Skill Items - 2 items per row on larger screen */}
           <Tilt
             key={category.title}
@@ -67,9 +72,9 @@ const Skills = () => (
                       scale: [1, 1.2, 1], // scales up and returns
                     }}
                     transition={{
-                      duration: 1,          // duration of one full scale cycle
-                      repeat: Infinity,     // repeat forever
-                      ease: 'easeInOut',    // smooth ease
+                      duration: 1, // duration of one full scale cycle
+                      repeat: Infinity, // repeat forever
+                      ease: "easeInOut", // smooth ease
                     }}
                   />
 
